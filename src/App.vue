50cff8aab6edb7da46a54fa19df669c8bd803c15
bycgxss <bycgxss@gmail.com>
2020-06-12 21:01:27 +0800

    创建 Header 组件

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header'
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+  #app {
+    background: #e1e1e1;
+  }
+  .main {
+    width: 90%;
+    margin: 0 auto;
+  }
+</style>
